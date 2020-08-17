aad189d58aa7e396d69096aaf9ca26af98bd4db3
Сергей Колесник <f1anor@yandex.ru>
2020-08-17 19:13:51 +0300

    final8

---
+++ precache-manifest.dad0a53c5b0234345351f4cf4e08c2f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be4fdf9599087e9146a24466df8534",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6bd1a846ef4f65f5b39",
+    "url": "/static/css/main.91d14999.chunk.css"
+  },
+  {
+    "revision": "dd8d447b869de9e8fa3c",
+    "url": "/static/js/2.85a2cebf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85a2cebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bd1a846ef4f65f5b39",
+    "url": "/static/js/main.70ac9b95.chunk.js"
+  },
+  {
+    "revision": "249c7d12a5f021d4a51a",
+    "url": "/static/js/runtime-main.2d67b12a.js"
+  },
+  {
+    "revision": "184a15468d9f96f17ebca26e7ba03140",
+    "url": "/static/media/click.184a1546.mp3"
+  }
+]);
